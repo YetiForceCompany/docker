79c2ad2097f7dda3ea5b13b6aac667ed240ce227
Antoni Kiszka <a.kiszka@yetiforce.com>
2023-07-23 18:26:46 +0200

    Update cron mounts

--- cron/cron-runner.sh
+++ cron/cron-runner.sh
@@ -3,8 +3,8 @@ set -euxo pipefail
 
 while true
 do
-	if test -f "/crm/config/Main.php"; then
-		unique_key=$(grep application_unique_key /crm/config/Main.php | awk -F"'" '{print $2}')
+	if test -f "/var/www/html/config/Main.php"; then
+		unique_key=$(grep application_unique_key /var/www/html/config/Main.php | awk -F"'" '{print $2}')
 		wget --timeout=900 -qO- http://nginx/cron.php?app_key="${unique_key}"
 	fi
 
